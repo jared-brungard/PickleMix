ee8942deebc01e3d8c703b388078f2f6b0ce3fa6
jared-brungard <jared.brungard.pro@gmail.com>
2026-02-23 19:20:14 -0700

    Initial Commit

---
+++ PickleMix/PickleMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickleMixApp.swift
+//  PickleMix
+//
+//  Created by Jared Brungard on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct PickleMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PickleMixTests/PickleMixTests.swift
@@ -0,0 +1,17 @@
+//
+//  PickleMixTests.swift
+//  PickleMixTests
+//
+//  Created by Jared Brungard on 2/23/26.
+//
+
+import Testing
+@testable import PickleMix
+
+struct PickleMixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
